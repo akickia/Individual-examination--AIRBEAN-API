a4f7ef71c17966cc0b55bdbe5ec907659dd3b4cf
adamsjodin <adamsjodin@hotmail.com>
2023-05-29 15:26:53 +0200

    created signup & login

--- app.js
+++ app.js
@@ -28,20 +28,38 @@ app.get('/api/users', async (request, response) =>{
     //zip code: zipCode,
     // city: city
     //}
+    // orders: {
+        //23489238
+    //}
 //}
 
 app.post('/api/signup', checkBody, async (request, response) => {
     const newUser = request.body;
-    const existingUser = await usersDb.findOne(user => user.username === newUser.username || user.email === newUser.email);
+    const existingUser = await usersDb.findOne({ $or: [{ username: newUser.username }, { email: newUser.email }] });
 
-    if (existingUser) {
-        console.log(existingUser);
-        response.status(400).json({ success: false, message: "Username or email already exists, please try to login or request new password" });
+    if (existingUser && existingUser.username === newUser.username) {
+        response.status(400).json({ success: false, message: "Username already exists, please try to login or request new password" });
+
+     } else if (existingUser && existingUser.email === newUser.email) {
+        response.status(400).json({ success: false, message: "Email already exists, please try to login or request new password" });
     } else {
         await usersDb.insert(newUser);
         response.send({ success: true, user: newUser});
     }
 });
+
+app.post('/api/login', async (request, response) => {
+    const user = request.body;
+    const existingUser = await usersDb.findOne({ $or: [{ username: user.username }]});
+
+    if (existingUser) {
+        response.send({ success: true,Message: "Welcome to beans", User: existingUser});
+    } else {
+        response.status(400).json({ success: false, message: "Wrong username or password, please try again!" });
+
+    }
+
+})
 
 app.listen(8000, () =>{
     console.log('App started on port 8000!!');
