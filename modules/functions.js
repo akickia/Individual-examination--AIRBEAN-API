d2a9ffb0dd37f99fffff295b67938d52e624a893
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-06-02 14:08:32 +0200

    moved funktion

---
+++ modules/functions.js
@@ -0,0 +1,20 @@
+const {usersDb} = require('./db');
+
+async function estimatedDelivery(userId) {
+  let currentTime = moment();
+  const user = await usersDb.findOne({ _id: userId });
+  if (user.orders) {
+      for(const [index, element] of user.orders.entries()) {
+          let deliveredTime = element.date;
+          console.log(deliveredTime)
+          let result =  currentTime.diff(deliveredTime, 'minutes');
+          console.log(result);
+          if (result >= 1 && !element.isDelivered) {
+              await usersDb.update({ _id: userId }, { $set: { [`orders.${index}.isDelivered`]: true } });
+          } 
+      };
+  }
+}
+
+
+module.exports =  { estimatedDelivery}
